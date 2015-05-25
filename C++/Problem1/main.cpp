abd6bcf91c28e9ee1d47e548f80decac09a46cc0
Forrest Pruitt <fpruitt0922@gmail.com>
2015-05-25 13:22:13 -0400

    Finishing Problem 1

--- C++/Problem1/main.cpp
+++ C++/Problem1/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Beginning..." << endl;
+    int sum = 0;
+    for(int i = 3; i<1000; i++)
+    {
+        if(i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+    cout << "The sum is "<<sum<<endl;
+    cout << "Terminating..." << endl;
     return 0;
 }
